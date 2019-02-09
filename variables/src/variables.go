6ca02048063fea8bbb5f94303f27db21ed28f320
edualb <eduardoslafiq@gmail.com>
2019-02-09 15:39:09 -0200

    Example 2 and 3 added.

--- variables/src/variables.go
+++ variables/src/variables.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 var (
@@ -18,6 +19,17 @@ var (
 	*	module float64
 	*
 	*****************************************
+	*
+	*					EXAMPLE 2
+	*	Continuing...
+	*
+	*	name, course, module = "Nidel", "Docker Deep Dive", 3.2
+	*
+	*****************************************
+	*
+	*	name = "Nigel"
+	*	course = "Docker Deep Dive"
+	*	module = 3.2
 	 */
 	name   string
 	course string
@@ -33,7 +45,33 @@ func main() {
 	*	fmt.Println("Module is set to", module)
 	*
 	*****************************************
+	*
+	*					EXAMPLE 2
+	*
+	*	fmt.Println("Name is set to", name, "and is of type", reflect.TypeOf(name))
+	*	fmt.Println("Module is set to", module, "and is of type", reflect.TypeOf(module))
+	*
+	*****************************************
+	*
+	*					EXAMPLE 3
+	*
+	*	a := 10.000000 // float
+	*	b := 3         //integer
+	*
+	*	fmt.Println("\nA is type", reflect.TypeOf(a), "and B is of type", reflect.TypeOf(b))
+	*
+	*	c := int(a) + b // 	We need cast the float number to calculate two integer numbers.
+	*					//	But the A variable keep itself float.
+	*
+	*	fmt.Println("\n has value", c, "and is of type", reflect.TypeOf(c))
 	 */
-	fmt.Println("Name is set to", name)
-	fmt.Println("Module is set to", module)
+
+	a := 10.000000 // float
+	b := 3         //integer
+
+	fmt.Println("\nA is type", reflect.TypeOf(a), "and B is of type", reflect.TypeOf(b))
+
+	c := int(a) + b // We need cast the float number to calculate two integer numbers.
+
+	fmt.Println("\n has value", c, "and is of type", reflect.TypeOf(c))
 }
