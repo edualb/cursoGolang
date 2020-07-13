6cd12d509cb9c250d1b17dccd1de666e1dc42e33
Edualb <eduardoslafiq@gmail.com>
2020-07-13 13:46:41 -0300

    testing modules

---
+++ modules/go.mod
@@ -0,0 +1,5 @@
+module github.com/edualb/cursoGolang
+
+go 1.14
+
+require rsc.io/quote v1.5.2
---
+++ modules/hello.go
@@ -0,0 +1,9 @@
+package modules
+
+import "rsc.io/quote"
+
+// Hello prints "Hello, world"
+func Hello() string {
+	return quote.Hello()
+	// return "Hello, world."
+}
